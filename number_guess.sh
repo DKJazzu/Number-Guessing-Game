3b5d06cb8c6be70571192ee4ee490e44f429226e
DKJazzu <dkjazzu@gmail.com>
2026-04-26 10:55:38 +0200

    fix: correct integer validation logic

--- number_guess.sh
+++ number_guess.sh
@@ -11,5 +11,11 @@ echo "Guess the secret number between 1 and 1000:"
 while true
 do
   read GUESS
+# non-integer check
+  if [[ ! "$GUESS" =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
   (( TRIES++ ))
 done
