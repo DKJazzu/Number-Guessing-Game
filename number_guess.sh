c419e1434e2c88364097f81ab36ad0d4a5f98bfd
DKJazzu <dkjazzu@gmail.com>
2026-04-26 11:20:37 +0200

    refactor: simplify guessing loop

--- number_guess.sh
+++ number_guess.sh
@@ -17,5 +17,17 @@ do
     echo "That is not an integer, guess again:"
     continue
   fi
-  (( TRIES++ ))
+  
+(( TRIES++ ))
+
+  if [[ "$GUESS" -eq "$SECRET_NUMBER" ]]
+  then
+    echo "You guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    break
+  elif [[ "$GUESS" -gt "$SECRET_NUMBER" ]]
+  then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
 done
