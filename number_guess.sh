74356065978546854a037e78c5cfbf3c0b6f5cf2
DKJazzu <dkjazzu@gmail.com>
2026-04-26 10:52:07 +0200

    feat: implement random number generation and guessing loop

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,14 @@
 
 echo "Enter your username:"
 read USERNAME
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+TRIES=0
+
+echo "Guess the secret number between 1 and 1000:"
+
+while true
+do
+  read GUESS
+  (( TRIES++ ))
+done
